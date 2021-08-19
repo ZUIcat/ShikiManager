8855c4e1c04eeec0e26dd634b8d534e5174e22d2
ZUIcat <35529568+ZUIcat@users.noreply.github.com>
2021-08-19 18:48:21 +0800

    none

--- HelperConfig/ShikiGameConfig.cs
+++ HelperConfig/ShikiGameConfig.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 namespace HelperConfig {
     public class ShikiGameConfig {
         public IList<string> Title { get; set; } // 恋愛×ロワイアル
-        public IList<string> Version { get; set; } // v1.02; rev 1623
+        public IList<string> Version { get; set; } // v1.02; rev 1623; adult v1.0
         public IList<string> Company { get; set; } // ASa Project
         public IList<string> Date { get; set; } // 20201127
         public IList<string> Package { get; set; } // Package/DL/Steam
-        public IList<string> Included { get; set; } // None
+        public IList<string> IncludedIn { get; set; } // None/某合集
         public IList<string> Description { get; set; } // 私、転校生である花丸まりは、小町ひろたかくんのことが好きですっ付き合ってください！
         public IList<string> Tag { get; set; } // 高森奈津美; 山岡ゆり
         public IList<string> Note { get; set; } // 有点无聊……别忘了还有俩 FD。
@@ -36,9 +36,11 @@ namespace HelperConfig {
     }
 
     public class ExeConfig {
-        public string Name { get; set; }
-        public string ExePath { get; set; }
-        public string SpecialCode { get; set; }
+        public string Name { get; set; } // 开始游戏
+        public string ExePath { get; set; } // ./yysy.exe
+        public string SpecialCode { get; set; } // #HDF:668t
+        public string SaveFolder { get; set; } // $appdata$/AAA/bbb
+        public int LocaleEmu { get; set; } // 0 为没有，1 为 LocaleEmulator，2 为 Ntleas
 
         public ExeConfig() {
         }
